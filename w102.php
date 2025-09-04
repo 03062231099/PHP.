efac4359e6a88548ff80363de8f7d0d736ac9176
03062231099 <0306231099@caothang.edu.vn>
2025-09-05 00:46:28 +0700

    Add files via upload

---
+++ w102.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <?php 
+    $a = 10;
+    $b = 7;
+   echo ("$a". "+" ."$b" ."=" . $a + $b . "\n" . "$a". "-" ."$b" ."=" . $a - $b ."\n" . "$a". "*" ."$b" ."=" . $a * $b . "\n" ."$a". "/" ."$b" ."=" . $a / $b . "\n". "$a". "%" ."$b" ."=" . $a % $b);
+  ?>  
+</body>
+</html>
---
+++ w107.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    for ($i = 1 ; $i <8 ; $i++)
+        for ($j = 1 ; $j <= 7 ; $j++)
+    {
+        echo $j*$i. "\n"; 
+    }    
+    ?>
+</body>
+</html>
