d4fbcb42023f7208ff16b9096733c72fabb418de
03062231099 <0306231099@caothang.edu.vn>
2025-09-19 01:59:35 +0700

    Add files via upload

---
+++ W1/03.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $variable = 8;
+    echo "value is now " . $variable;
+    echo "Add 2. value is now " . $variable += 2;
+    echo "Subtract 4. value is now " . $variable -= 4;
+    echo "Multiphy by 5. value is now " .$variable *= 5;
+    echo "divide by 3. value is now " . $variable /= 10;
+    echo "Increment value by one. value is now " . $variable = $variable++;
+    echo "Decrement value by one. value is now " . $variable = $variable--;
+    ?>
+</body>
+</html>
